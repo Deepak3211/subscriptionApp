4820bd7f588ec8131780b8eafd0b27b4f5136394
deepak3211 <deepakt754@gmail.com>
2021-12-13 23:33:33 +0530

    deployment

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'PRODUCTION') {
-    app.use(express.static(path.join(__dirname, './client/build')))
+    app.use(express.static('client/build'))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, './client/build/index.html'))
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
