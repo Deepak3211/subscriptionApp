aa2308f4d2f4de8b4af0bdf6df4ff1a622461a01
deepak3211 <deepakt754@gmail.com>
2021-12-14 01:06:25 +0530

    deployment

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ app.use('/api/v1', subscriptionRouter);
 connectDB();
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
+    app.use(express.static('./client/build'))
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, './client', 'build', 'index.html'))
     })
 }
 
